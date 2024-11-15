0dc49b1e5c2c9b56817d18429e693ca4b3f9e5f1
tilok <tilakthexp@gmail.com>
2024-11-15 14:46:10 +0530

    userModel updated

--- models/userModel.js
+++ models/userModel.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
-  year_of_study: { type: Number },
+  year_of_study: { type: String },
   role: {
     type: String,
     default: "user",
