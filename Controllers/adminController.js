253de754651600d8f3472b7834fac8d8e57d3d8f
Tilok doley <tilakthenoob@gmail.com>
2024-11-25 11:35:48 +0530

    admin route added

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,15 +1,22 @@
 const User = require("../models/userModel")
 
 const getAllUser = async(req,res) => {
-    const {role} = req.body
+    const {_id} = req.body
     if(!role || role!="admin"){
         return res.status(400).json({success: false, message: "All fields are required"})
     }
     try{
+    const admin = await User.findOne({_id})
+    if(admin.role !== "admin")
+    {
+        return res.status(400).json({success: false, message: "Unauthorized admin"})
+
+    }
     const user = await User.find()
     if(!user){
         return res.status(400).json({success: false, message: "No user found"})
     }
+
 
 
     res.status(200).json({success: true, user})
