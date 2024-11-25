41ee56d411b1b201583989419990c717fe102690
Tilok doley <tilakthenoob@gmail.com>
2024-11-25 10:56:39 +0530

    admin login added

--- Routes/openRoute.js
+++ Routes/openRoute.js
@@ -2,4 +2,7 @@ const router = require("express").Router();
 const GetController = require("../Controllers/ItemsController");
 router.get("/get-products", GetController.getAllProducts);
 
+/*Admin endpoints*/
+
+router.post("/getallusers", Admin.getAllUser)
 module.exports = router;
