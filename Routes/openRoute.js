df456327ce54cda3b6047b937678cb2cc1200dc4
Tilok doley <tilakthenoob@gmail.com>
2024-11-25 11:00:30 +0530

    bug fixed

--- Routes/openRoute.js
+++ Routes/openRoute.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const GetController = require("../Controllers/ItemsController");
+const Admin = require("../Controllers/adminController")
 router.get("/get-products", GetController.getAllProducts);
 
 /*Admin endpoints*/
